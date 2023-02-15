5d98747cc08e003eae84ec4b57d20913ceb0796b
Coldsky22 <bingalik@mail.ru>
2023-02-15 04:15:50 +0300

    ddd

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,32 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import (
+    SignupView,
+    MeView,
+    UserViewSet,
+    TokenView,
+)
+router = routers.DefaultRouter()
+router.register(
+    r'users',
+    UserViewSet,
+    basename='user',
+)
+
+# urlpatterns = [
+#     path('auth/token/', TokenView.as_view(),
+#          name='create_token'),
+#     path('auth/signup/', SignupView.as_view(), name='signup'),
+#     path('users/me/', MeView.as_view(), name='me'),
+#     path('', include(router.urls)),
+# ]
+
+urlpatterns = [
+    path('v1/users/me/', MeView.as_view(), name='users'),
+    path('v1/users/<slug:username>/', UserViewSet.as_view()),
+    path('v1/users/me/', MeView.as_view(), name='me'),
+    path('v1/auth/signup/', SignupView.as_view(), name='sign_up'),
+    path('v1/auth/token/', TokenView.as_view(), name='activation'),
+    path('v1/', include(router.urls)),
+]
