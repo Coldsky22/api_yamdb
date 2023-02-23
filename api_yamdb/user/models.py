8e18bc02702cf43b453a09fcd9f52503e7b6b671
Stanislav Kirkin <skirkin@mail.ru>
2023-02-23 21:32:39 +0300

    С15.Kirkin. Правки ревьюера. Часть 3.

--- api_yamdb/user/models.py
+++ api_yamdb/user/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
-from django.contrib.auth.validators import UnicodeUsernameValidator
-from .validators import me_username
+
 
 class UserRole(models.TextChoices):
     ADMIN = 'admin'
@@ -38,7 +37,7 @@ class User(AbstractUser):
         verbose_name='E-mail',
     )
     confirmation_code = models.CharField(
-        max_length=20,
+        max_length=100,
         blank=True,
         null=True,
         verbose_name='Код подтверждения',
